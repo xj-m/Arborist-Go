d104064b9ca8ba9537a7193ba0bfe2b4fbcea014
马向君 <xiangjun@maxiangjuns-MacBook-Pro.local>
2019-09-13 19:55:49 -0400

    first c

---
+++ app.js
@@ -0,0 +1,46 @@
+let createError = require('http-errors');
+let express = require('express');
+let firebase = require('firebase');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+
+// firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyCwMpSpHHrjLaiAHR0mgB6uZcjeccuW6Jg",
+  authDomain: "hophacks-68e28.firebaseapp.com",
+  databaseURL: "https://hophacks-68e28.firebaseio.com",
+  projectId: "hophacks-68e28",
+  storageBucket: "hophacks-68e28.appspot.com",
+  messagingSenderId: "349264057221",
+  appId: "1:349264057221:web:433a09049ce52578b0aaec"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let indexRouter = require('./controllers/index');
+const errorController = require('./controllers/error');
+
+let app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// error handler
+app.use(errorController.get404);
+
+// Setting up port
+const port = process.env.PORT || 1109;
+
+app.listen(port, function () {
+  console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", port, port);
+});
+module.exports = app;
